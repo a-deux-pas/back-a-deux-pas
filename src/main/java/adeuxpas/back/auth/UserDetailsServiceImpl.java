909bc23ab1cb0102eb6141ff0e85e14183de3d4a
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-02-19 19:43:21 +0100

    implemented Spring Security's UserDetailsService interface

--- src/main/java/adeuxpas/back/auth/UserDetailsServiceImpl.java
+++ src/main/java/adeuxpas/back/auth/UserDetailsServiceImpl.java
@@ -1,5 +1,42 @@
 package adeuxpas.back.auth;
 
-public class UserDetailsServiceImpl {
+import adeuxpas.back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(@Autowired UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<adeuxpas.back.entity.User> userInDB = this.userRepository.findByEmail(email);
+        if (userInDB.isPresent()) {
+            adeuxpas.back.entity.User user = userInDB.get();
+            Set<GrantedAuthority> authorities = new HashSet<>();
+            authorities.add(new SimpleGrantedAuthority("" + user.getRole()));
+
+            return User
+                    .withUsername(user.getEmail())
+                    .password(user.getPassword())
+                    .authorities(authorities)
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
 }
