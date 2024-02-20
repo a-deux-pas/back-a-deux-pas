02fec432847a617da395d2f79cf740488c1dde9d
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-02-20 12:21:28 +0100

    crated the Security Configuration

--- src/main/java/adeuxpas/back/config/SecurityConfig.java
+++ src/main/java/adeuxpas/back/config/SecurityConfig.java
@@ -1,4 +1,42 @@
 package adeuxpas.back.config;
 
+import adeuxpas.back.auth.JWTFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JWTFilter jwtFilter;
+
+    public SecurityConfig(@Autowired JWTFilter jwtFilter){
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configure(http))
+                .csrf(CsrfConfigurer::disable) // disable csrf during development
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/signup", "/api/login").permitAll()
+                        .requestMatchers("/api/content").hasAnyAuthority("USER", "ADMIN")
+                        .requestMatchers("/api/admin-page").hasAuthority("ADMIN"))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        // adding our custom JWTFilter to the SecurityFilterChain
+        http.addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
