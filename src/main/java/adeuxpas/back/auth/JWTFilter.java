7ab01aef4af4713ff560cb2fce900de1ce18333a
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-02-20 12:18:56 +0100

    created JWT custom filter and its service class

--- src/main/java/adeuxpas/back/auth/JWTFilter.java
+++ src/main/java/adeuxpas/back/auth/JWTFilter.java
@@ -1,5 +1,56 @@
 package adeuxpas.back.auth;
 
-public class JWTFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.Date;
+
+
+// Custom created filter that verifies the presence and validity of the JWT in the Authorization Header, inside the request
+// sent by the client, before processing it
+// Runs once for each request when we add it to the FilterChain
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JWTFilter(@Autowired JWTService jwtService,
+                     @Autowired UserDetailsService userDetailsService){
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null){
+            String token = authorizationHeader.substring(7);
+            UserDetails user = this.userDetailsService.loadUserByUsername(this.jwtService.getSub(token));
+
+            if (this.jwtService.getExpiration(token).after(new Date())){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword(),
+                        user.getAuthorities()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/adeuxpas/back/auth/JWTService.java
+++ src/main/java/adeuxpas/back/auth/JWTService.java
@@ -1,5 +1,53 @@
 package adeuxpas.back.auth;
 
+import adeuxpas.back.enums.UserRole;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+// Service class for the JWT
+// Contains methods to generate, get the validity and the subject of the JWT
+@Service
 public class JWTService {
 
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private int jwtExpiration;
+
+    public String generateToken(String username, UserRole role) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("user", role.equals(UserRole.USER))
+                .claim("admin",role.equals(UserRole.ADMIN))
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + jwtExpiration))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .compact();
+    }
+
+    public String getSub(String token){
+        return Jwts
+                .parser()
+                .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public Date getExpiration(String token){
+        return Jwts
+                .parser()
+                .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
 }
