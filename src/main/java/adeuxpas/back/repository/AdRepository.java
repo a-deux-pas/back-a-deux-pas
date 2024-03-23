31a66bfd93c97e7d7ad3e82f2bd4ff79f9d2fe9d
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-03-23 15:48:52 +0100

    implemented logic for the article state and postal code filters

--- src/main/java/adeuxpas/back/repository/AdRepository.java
+++ src/main/java/adeuxpas/back/repository/AdRepository.java
@@ -10,23 +10,18 @@ import java.util.List;
 
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Long> {
-    @Query("SELECT a FROM Ad a JOIN User u WHERE " +
-                "(:postalCodes IS NULL OR u.postalCode IN :postalCodes) AND " +
-                "(:articleStates IS NULL OR a.articleState IN :articleStates) AND " +
-                "(:categories IS NULL OR a.category IN :categories) AND " +
-                "(:subcategories IS NULL OR a.subcategory IN :subcategories) AND " +
-                "(:gender IS NULL OR a.articleGender IN :gender) AND" +
-                "(:priceRanges IS NULL OR " +
-                    "a.price >= :minPrice1 AND a.price < :maxPrice1 OR " +
-                    "a.price >= :minPrice2 AND a.price < :maxPrice2 OR " +
-                    "a.price >= :minPrice3 AND a.price < :maxPrice3 OR " +
-                    "a.price >= :minPrice4 AND a.price < :maxPrice4 OR " +
-                    "a.price >= :minPrice5 AND a.price < :maxPrice5)"
+    @Query("SELECT a FROM Ad a JOIN User u ON a.publisher = u WHERE (:postalCodes IS NULL OR u.postalCode IN :postalCodes) AND (:articleStates IS NULL OR a.articleState IN :articleStates)"
             )
+  /*@Query(nativeQuery = true, value = "SELECT a.* FROM ad a JOIN user u ON a.publisher_id = u.id WHERE " +
+           "((:postalCodes is null OR u.postal_code IN :postalCodes) AND :articleStates is null ) OR" +
+          "((:postalCodes is null OR u.postal_code IN :postalCodes) AND a.article_state IN :articleStates )"
+
+   )*/
+
     List<Ad> findFilteredAds(@Param("postalCodes") List<String> postalCodes,
-                             @Param("priceRanges") List<String> priceRanges,
-                             @Param("articleStates") List<String> articleStates,
+                            // @Param("priceRanges") List<String> priceRanges,
+                             @Param("articleStates") List<String> articleStates/*,
                              @Param("categories") List<String> categories,
                              @Param("subcategories") List<String> subcategories,
-                             @Param("gender") List<String> gender);
+                             @Param("gender") List<String> gender*/);
 }
--- src/main/java/adeuxpas/back/service/AdServiceImpl.java
+++ src/main/java/adeuxpas/back/service/AdServiceImpl.java
@@ -7,6 +7,7 @@ import adeuxpas.back.repository.AdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,15 +33,38 @@ public class AdServiceImpl implements AdService{
     }
 
     @Override
-    public List<HomePageAdDTO> findFilteredAds(List<String> selectedPriceRanges, List<String> citiesAndPostalCodes, List<String> selectedArticleStates, List<String> selectedCategories, List<String> selectedSubcategories, List<String> selectedGender) {
+    public List<HomePageAdDTO> findFilteredAds(List<String> selectedPriceRanges, List<String> citiesAndPostalCodes,
+                                               List<String> selectedArticleStates, List<String> selectedCategories,
+                                               List<String> selectedSubcategories, List<String> selectedGender) {
+
+        // if no filter is checked, return all ads
+        if(selectedPriceRanges.isEmpty() && citiesAndPostalCodes.isEmpty() &&
+                selectedArticleStates.isEmpty() && selectedCategories.isEmpty() &&
+                selectedSubcategories.isEmpty() && selectedGender.isEmpty())
+            return this.findAllHomePageAds();
+
         // Formatting the request parameters (received from the controller) to be passed as query parameters (to the repository)
-        // extracting the postal codes :
+        // extracting the postal codes
         List<String> postalCodes = citiesAndPostalCodes.stream().map(city -> city.substring(city.indexOf('(') + 1, city.indexOf(')')))
                 .collect(Collectors.toList());
         postalCodes.forEach(System.out::println);
 
-        List<Ad> myAds = this.adRepository.findFilteredAds(selectedPriceRanges, postalCodes, selectedArticleStates, selectedCategories, selectedSubcategories, selectedGender);
-        List<HomePageAdDTO> mappedAdsList = myAds.stream().map(mapper::adToHomePageAdDTO).collect(Collectors.toList());
+        // preparing the price ranges
+        BigDecimal minPrice1;
+        BigDecimal maxPrice1;
+        BigDecimal minPrice2;
+        BigDecimal maxPrice2;
+        BigDecimal minPrice3;
+        BigDecimal maxPrice3;
+        BigDecimal minPrice4;
+        BigDecimal maxPrice4;
+        BigDecimal minPrice5;
+        BigDecimal maxPrice5;
+        BigDecimal minPrice6;
+        BigDecimal maxPrice6;
+
+        List<Ad> myFilteredAds = this.adRepository.findFilteredAds(postalCodes.isEmpty()? null: postalCodes, selectedArticleStates.isEmpty() ? null :selectedArticleStates);
+        List<HomePageAdDTO> mappedAdsList = myFilteredAds.stream().map(mapper::adToHomePageAdDTO).collect(Collectors.toList());
         if(mappedAdsList.size() > 1) this.sortByCreationDateDesc(mappedAdsList);
 
         return mappedAdsList;
