cb2392689882e4a0f415ad2155d6c2b3fe882860
LEA HADIDA <hadida.lea@gmail.com>
2024-04-03 20:51:35 +0200

    added method to the BackApplicationTests

--- src/test/java/adeuxpas/back/BackApplicationTests.java
+++ src/test/java/adeuxpas/back/BackApplicationTests.java
@@ -1,11 +1,14 @@
 package adeuxpas.back;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class BackApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "Context loaded successfully");
         System.out.println("Test successful");
 	}
 }
