e14d886b5fce4f2a9d724788e1d1c4d6fecf8bd3
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-04-19 18:56:39 +0200

    continuing CI test fails bebugging

--- src/test/java/adeuxpas/back/BackApplicationTests.java
+++ src/test/java/adeuxpas/back/BackApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 
+@SpringBootTest
 class BackApplicationTests {
 
 	/*@Test
