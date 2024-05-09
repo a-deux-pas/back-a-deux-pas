fb8b6dfbb967f277a6abc3885a1904e9e47412de
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-05-09 12:45:58 +0200

    removed unused constructor and method

--- src/main/java/adeuxpas/back/dto/PreferredScheduleDTO.java
+++ src/main/java/adeuxpas/back/dto/PreferredScheduleDTO.java
@@ -24,26 +24,6 @@ public class PreferredScheduleDTO {
     private String endTime;
     private Long userId;
 
-    public PreferredScheduleDTO() {
-    }
-
-    /**
-     * Constructor for PreferredScheduleDTO with all attributes.
-     *
-     * @param id          The unique identifier for the preferred schedule.
-     * @param daysOfWeek  The days of the week for the preferred schedule.
-     * @param startTime   The start time of the preferred schedule.
-     * @param endTime     The end time of the preferred schedule.
-     * @param userId      The ID of the user associated with the preferred schedule.
-     */
-    public PreferredScheduleDTO(Long id, List<Integer> daysOfWeek, LocalTime startTime, LocalTime endTime, Long userId) {
-        this.id = id;
-        this.daysOfWeek = daysOfWeek;
-        this.startTime = formatTime(startTime);
-        this.endTime = formatTime(endTime);
-        this.userId = userId;
-    }
-
     public Long getId() {
         return id;
     }
@@ -82,16 +62,6 @@ public class PreferredScheduleDTO {
 
     public void setUserId(Long userId) {
         this.userId = userId;
-    }
-    
-    /**
-     * Formats a LocalTime object to a String representation in "HH:mm" format.
-     *
-     * @param time The LocalTime object to be formatted.
-     * @return The formatted time string.
-     */
-    private String formatTime(LocalTime time) {
-        return time.format(DateTimeFormatter.ofPattern("HH:mm"));
     }
 }
 
