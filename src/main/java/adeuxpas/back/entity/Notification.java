1e979f8e97edc4800ff92a08edc5174c15ab3d13
LEA HADIDA <hadida.lea@gmail.com>
2024-06-22 15:11:17 +0200

    removed unused import

--- src/main/java/adeuxpas/back/entity/Notification.java
+++ src/main/java/adeuxpas/back/entity/Notification.java
@@ -1,18 +1,7 @@
 package adeuxpas.back.entity;
 
 import adeuxpas.back.enums.EventNames;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 /**
  * Entity representing a user's notifications preferences.
