579a439d4e44c946bfc4a91383364dbea4bb5282
Mircea Bardan <mircea.c.bardan@gmail.com>
2024-07-31 02:46:43 +0200

    refined permitted urls list, updtaed .gitignore to include the SSL cert

--- src/main/java/adeuxpas/back/config/SecurityConfig.java
+++ src/main/java/adeuxpas/back/config/SecurityConfig.java
@@ -48,19 +48,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Configure the default Cross-Origin Resource Sharing (CORS) policy for the
+                // Configure the Cross-Origin Resource Sharing (CORS) policy for the
                 // HTTP security.
-                // Without the '.cors(cors -> cors.configure(http))' line, the default CORS configuration provided by Spring Security
-                // would still be in effect,
-                // permitting same-origin resource sharing and denying cross-origin requests:
-                // .cors(cors -> cors.configure(http))
-                // The explicit inclusion of this line simply ensures that the default
-                // configuration is applied explicitly
-                // and provides a hook for further customization, needed in the near future:
-                // FOR EXAMPLE, we'll need to customize the above default CORS configuration,
-                // when we start making requests to our Front End,
-                // to only allow cross-origin requests from our Angular web server, running at
-                // localhost:4200, like so:
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(List.of("https://localhost:4200")); // self-explanatory
@@ -68,8 +57,6 @@ public class SecurityConfig {
                     // For ex: standard headers like Content-Type, Authorization, etc.,
                     // but also custom headers that the frontend application might include in its
                     // requests.
-                    // In a production environment it's more secure to explicitly specify the
-                    // headers that are allowed, rather than allowing them all with the * symbol.
                     configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
                     return configuration;
                 }))
@@ -79,27 +66,22 @@ public class SecurityConfig {
                 .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         // We explicitly define all the endpoints that we permit open access to.
+                        // (some of them might seem that they shouldn't belong in this list, but they are needed, as per our business logic)
                         // This practice complies with the 'Default Deny'/'Implicit Deny' principle, that is a fundamental aspect
                                 // of security practices and advises that, by default,
                                 // all access should be denied unless explicitly allowed.
                         // Open access routes (to be verified/updated) :
                         .requestMatchers(
-                                "/api/account/create",
                                 "/api/auth/**",
-                                "/api/ads/{id}",
-                                "/api/ads/adTablist/{userId}",
-                                "/api/ads/favorites/{userId}",
                                 "/api/ads/{adId}/{loggedInUserId}",
                                 "/api/ads/adPageContentList/{publisherId}/{loggedInUserId}/{adId}",
                                 "/api/ads/similarAdsList/{category}/{publisherId}/{userId}",
                                 "/api/ads/list",
                                 "/api/users/cities-and-postal-codes",
-                                "/api/users/{id}/alias-and-location",
-                                "/api/users/{userAlias}",
-                                "/api/users/{userAlias}/presentation",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api/webhooks/**"
                         ).permitAll()
                         // Any other request must be authenticated
                         .anyRequest().authenticated()
